f50fffcd27e5ee93ae029a99fc89db21424b8737
Julia St.Germain <juliastgermain1125@gmail.com>
2022-02-02 16:09:50 -0500

    Added code for Intake In, Out, and Stop in the command

--- src/main/java/frc/robot/commands/Cmd_IntakeElevUP.java
+++ src/main/java/frc/robot/commands/Cmd_IntakeElevUP.java
@@ -49,6 +49,7 @@ public class Cmd_IntakeElevUP extends CommandBase {
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
+        m_sub_IntakeElev.IntakeElevIn();
     }
 
     // Called every time the scheduler runs while the command is scheduled.
@@ -59,6 +60,7 @@ public class Cmd_IntakeElevUP extends CommandBase {
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
+        m_sub_IntakeElev.IntakeElevStop();
     }
 
     // Returns true when the command should end.
