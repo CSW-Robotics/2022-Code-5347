46cd28c20f41ebc745b02772484f8115e9c26dd5
Aidan <aalvarado2025@csw.org>
2022-02-02 15:42:44 -0500

    add climb down and climb stop to climbing down

--- src/main/java/frc/robot/commands/Cmd_ClimbingDown.java
+++ src/main/java/frc/robot/commands/Cmd_ClimbingDown.java
@@ -49,11 +49,13 @@ public class Cmd_ClimbingDown extends CommandBase {
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
+        m_sub_Climbing.climbdown();
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
+        m_sub_Climbing.climbstop();
     }
 
     // Called once the command ends or is interrupted.
